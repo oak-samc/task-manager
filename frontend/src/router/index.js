6d006e8fa7fd41d83f17ae2dc43822ba15ed1faf
OakSamc <samuelbfmv@gmail.com>
2025-10-19 19:47:34 -0300

    configuração do Vue Router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ProjectsView from '@/views/ProjectsView.vue'
+import TasksView from '@/views/TasksView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'projects',
+      component: ProjectsView
+    },
+    {
+      path: '/tasks',
+      name: 'tasks',
+      component: TasksView
+    },
+    {
+      path: '/projects/:projectId/tasks',
+      name: 'project-tasks',
+      component: TasksView
+    }
+  ],
 })
 
 export default router
