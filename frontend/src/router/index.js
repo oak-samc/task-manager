20808ce416d322de7f7042de985ae936ba76f8cc
Samuel Carvalho <samuelbfmv@gmail.com>
2025-11-06 12:49:45 -0300

    modificação: redirecionar '/' e ajustar rotas

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,22 +2,37 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ProjectsView from '@/views/ProjectsView.vue'
 import TasksView from '@/views/TasksView.vue'
 
+// Definir rotas da aplicação
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/projects',
       name: 'projects',
       component: ProjectsView
     },
     {
-      path: '/tasks',
-      name: 'tasks',
+      path: '/',
+      redirect: '/projects'
+    },
+    {
+      path: '/alltasks',
+      name: 'alltasks',
       component: TasksView
+    },
+    {
+      path: '/tasks',
+      redirect: '/alltasks'
     },
     {
       path: '/projects/:projectId/tasks',
       name: 'project-tasks',
+      component: TasksView
+    },
+    {
+      path: '/projects/:projectId/alltasksproject',
+      name: 'project-alltasks',
       component: TasksView
     }
   ],
